d7e64570e2ffb4c0b04fc843b5e5d083daeee279
GuillermoSM5 <guillermosotomarquez5@gmail.com>
2025-06-24 16:26:45 -0600

    create a post model

---
+++ models/post.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.sql.functions import func
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+
+class Post(Base):
+    __tablename_ = "posts"
+
+    id_post = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    title = Column(String(255), nullable=False, index=True)
+    content = Column(String(1500), nullable=False)
+    image_url = Column(String(255), nullable=True)
+    id_autor = Column(Integer, ForeignKey("users.id_user"), nullable=False)
+    published = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=func.now())
+    update_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+    autor = relationship("User", back_populates="posts")
---
+++ schemas/post.py
@@ -0,0 +1,3 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, Field, field_validator, ConfigDict
