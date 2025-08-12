272875229f93854d4baa0a9363608bc5c3618fd3
GuillermoSM5 <guillermosotomarquez5@gmail.com>
2025-08-12 10:58:27 -0600

    feat(core/config): add cloudinary const

--- app/core/config.py
+++ app/core/config.py
@@ -16,6 +16,9 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCES_TOKEN_EXPIRE_MINUTES: int = 5
+    CLOUDINARY_CLOUD_NAME: str
+    CLOUDINARY_API_KEY: str
+    CLOUDINARY_API_SECRET: str
 
 
 settings = Settings()
